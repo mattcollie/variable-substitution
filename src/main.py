585784f055ec51f647c4c9b2e72ecf8cc2e03689
Matt Collecutt <mattcollie@live.com>
2024-05-07 23:50:16 +1200

    Replace print statements with logging in main.py
    In the main.py file, print statements were substituted for logging calls. In addition, a basic logging configuration was also set up. This change improves flexibility and information tracking in the log files.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import os
+import logging
+
 
 def main():
-    print("I ran")
-    print("ENV", os.environ)
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug("I ran")
+    logging.debug(f"ENV: {os.environ}")
 
 
 if __name__ == '__main__':

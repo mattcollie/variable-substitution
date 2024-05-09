1f2a53711bc43b967e9289294839f2b5663f1cd2
Matt Collecutt <mattcollie@live.com>
2024-05-09 15:23:09 +1200

    Refactor codebase for Docker compatibility and add new workflows
    The update modifies the main Python script to work with Docker and alters the action.yml file to utilize a Docker image. Test files and workflows have been removed to streamline the codebase. It also includes the addition of two new GitHub workflows: lint.yml for code linting, and integration.yml for integration testing along with a new Dockerfile for containerization.

--- src/main.py
+++ src/main.py
@@ -11,23 +11,23 @@ def main(file: str):
             logging.critical(f"'{file}' does not exist.")
             sys.exit(1)
 
-        logging.info(f'Processing file: {file}')
-        with open(file, 'r') as f:
+        logging.info(f"Processing file: {file}")
+        with open(file, "r") as f:
             data = json.load(f)
 
         for key, value in data.items():
             data = process_item(key, value, data)
 
-        with open(file, 'w') as f:
+        with open(file, "w") as f:
             json.dump(data, f, indent=2)
 
-        with open(file, 'r') as f:
+        with open(file, "r") as f:
             data = json.load(f)
 
         check_for_unsubstituted_variables(data)
         logging.info(f"All variables substituted successfully for: {file}!")
     except Exception as e:
-        logging.critical(f'Failed to process file: {file}; {e}')
+        logging.critical(f"Failed to process file: {file}; {e}")
         sys.exit(1)
 
 
@@ -40,10 +40,14 @@ def process_item(key, value, data) -> dict:
         for var_name in matches:
             env_value = os.environ.get(var_name)
             if env_value:
-                value = re.sub(rf"\$\{{\{{\s*{var_name}\s*\}}\}}", env_value, value)
+                value = re.sub(
+                    rf"\$\{{\{{\s*{var_name}\s*\}}\}}", env_value, value
+                )
                 data[key] = value
             else:
-                logging.warning(f"Warning: Environment variable '{var_name}' not found")
+                logging.warning(
+                    f"Warning: Environment variable '{var_name}' not found"
+                )
     elif isinstance(value, list):
         for v in value:
             process_item(None, v, value)
@@ -71,9 +75,6 @@ def check_for_unsubstituted_variables(data):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
-    if len(sys.argv) != 2:
-        logging.critical("Usage: python main.py <json_files>")
-        sys.exit(1)
-
-    for file in [f.strip() for f in sys.argv[1].split(',')]:
+    files = os.environ.get('INPUT_FILES')
+    for file in [f.strip() for f in files.split(",")]:
         main(file)
